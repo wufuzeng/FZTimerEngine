f0493a3d974a2ae00c12f308420e6dd04a1e58be
吴福增 <wufuzeng_lucky@sina.com>
2019-07-30 16:21:34 +0800

    wfz

--- Example/FZTimerEngine/FZViewController.m
+++ Example/FZTimerEngine/FZViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "FZViewController.h"
-
+#import "FZTimerEngine.h"
 @interface FZViewController ()
 
 @end
@@ -17,13 +17,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[FZTimerEngine sharedTimer] registerAction:^(NSTimeInterval timeinterval) {
+        NSLog(@"%ld",(long)timeinterval);
+    } frequency:10 name:@"key1"];
+    
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[FZTimerEngine sharedTimer] removeActionForName:@"key1"];
+    });
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
---
+++ Example/Pods/Target Support Files/FZTimerEngine/FZTimerEngine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FZTimerEngine : NSObject
+@end
+@implementation PodsDummy_FZTimerEngine
+@end
